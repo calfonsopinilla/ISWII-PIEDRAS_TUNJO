02b9055c748cfd0ff505fa88a2d348317a465e6a
Steven Cruz <blackjackers17@gmail.com>
2020-03-04 18:37:15 -0500

    Cambiando nombre carpeta

---
+++ Core-PiedrasDelTunjo/Logica/LUser.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilitarios;
+
+namespace Logica
+{
+    public class LUser
+    {
+        public List<UUser> GetUsers()
+        {
+            return new DaoUser().GetUsers();
+        }
+    }
+}
---
+++ Core-PiedrasDelTunjo/Utilitarios/UUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilitarios
+{
+    [Table("users", Schema = "public")]
+    public class UUser
+    {
+        [Key]
+        [Column("id")]
+        public int ID { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
