acb2258d922aebdaa02c231e01725dfb9514daf6
Steven Cruz <blackjackers17@gmail.com>
2020-03-04 19:55:40 -0500

    Base Core

---
+++ Core API/Data/DaoUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilitarios;
+
+namespace Data
+{
+    public class DaoUser
+    {
+        public List<UUser> GetUsers()
+        {
+            using (var db = new Mapeo())
+            {
+                return db.Usuarios.ToList();
+            }
+        }
+    }
+}
