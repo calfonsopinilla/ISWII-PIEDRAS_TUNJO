df5f99fe24646b451e961c7f298b343072974bdd
daniel zambrano <cdaniel1899@hotmail.com>
2020-03-10 00:53:39 -0500

    simulacion pantallas inicio , login

--- AngularWeb/src/app/app.module.ts
+++ AngularWeb/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { ContactanosComponent } from './componentes/contactanos/contactanos.component';
+import { GaleriaComponent } from './componentes/galeria/galeria.component';
+import { SeccionInformativaComponent } from './componentes/seccion-informativa/seccion-informativa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    LoginComponent,
+    TableroComponent,
+    PiePaginaComponent,
+    ConfiguracionComponent,
+    ContactanosComponent,
+    GaleriaComponent,
+    SeccionInformativaComponent
   ],
   imports: [
     BrowserModule,
