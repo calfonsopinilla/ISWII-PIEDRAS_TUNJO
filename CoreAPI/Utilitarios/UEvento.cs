a8fb6b38b9028e8580ae48f8d2c9c15bc34d42ee
Steven Cruz <blackjackers17@gmail.com>
2020-03-11 00:55:37 -0500

    configure 2

---
+++ CoreAPI/Utilitarios/UEvento.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Utilitarios {
+
+    [Serializable] // Se declara que la clase U Usuario se puede expresar en formato JSON
+    [Table("evento", Schema = "parque")] // Se específica la tabla con la que se relaciona la clase U Usuario
+    public class UEvento {
+
+        // Variables
+        private int id;
+        private string nombre;
+        private DateTime fechaPublicacion;
+        private DateTime fecha;
+        private string descripcion;
+        private double calificacion;
+        private string imagenesUrl;
+        private List<string> comentariosId;
+        private List<UComentarioEvento> listaComentariosEvento;
+
+        [Key]
+        [Column("id")]
+        public int Id { get => id; set => id = value; }
+        [Column("nombre")]
+        public string Nombre { get => nombre; set => nombre = value; }
+        [Column("fecha_publicacion")]
+        public DateTime FechaPublicacion { get => fechaPublicacion; set => fechaPublicacion = value; }
+        [Column("fecha")]
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        [Column("descripcion")]
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        [Column("calificacion")]
+        public double Calificacion { get => calificacion; set => calificacion = value; }
+        [Column("imagenes_url")]
+        public string ImagenesUrl { get => imagenesUrl; set => imagenesUrl = value; }
+        [Column("comentarios_id")]
+        public List<string> ComentariosId { get => comentariosId; set => comentariosId = value; }
+        [NotMapped]
+        public List<UComentarioEvento> ListaComentariosEvento { get => listaComentariosEvento; set => listaComentariosEvento = value; }
+    }
+}
---
+++ CoreAPI/WebSite/PiedrasDelTunjo/PiedrasDelTunjo/Controllers/EventosController.cs
@@ -0,0 +1,43 @@
+﻿using Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+
+namespace PiedrasDelTunjo.Controllers
+{
+    [RoutePrefix("eventos")]
+    public class EventosController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult ObtenerEventos()
+        {
+            var eventos = new LEvento().ObtenerEventos();
+            eventos = eventos.Where(x => x.Fecha <= DateTime.Now.AddMonths(1))
+                            .ToList();
+            return Ok(eventos); // Status 200 OK
+        }
+
+        [HttpGet]
+        [Route("obtenerImagen")]
+        // GET: /obtenerImagen/?nombre=imageName.jpg
+        public HttpResponseMessage ObtenerImagen([FromUri] string nombre)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            var path = $"~/Imagenes/Eventos/{nombre}";
+            path = System.Web.Hosting.HostingEnvironment.MapPath(path);
+            var ext = Path.GetExtension(path);
+            ext = ext.Substring(1); // para que elimine el punto (.) de la extensión
+            var contents = File.ReadAllBytes(path);
+
+            MemoryStream ms = new MemoryStream(contents);
+            response.Content = new StreamContent(ms);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue($"image/{ext}");
+            return response;
+        }
+    }
+}
