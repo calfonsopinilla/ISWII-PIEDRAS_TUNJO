4e76ea0925b46930b95264b1521d0de46ecdf560
Steven Cruz <blackjackers17@gmail.com>
2020-03-17 15:19:21 -0500

    Inicio y pages de incio con el nuevo servidor

--- Ionic App/src/app/pages/inicio/inicio.page.ts
+++ Ionic App/src/app/pages/inicio/inicio.page.ts
@@ -9,7 +9,7 @@ import { InfoParqueService } from '../../services/info-parque.service';
 })
 export class InicioPage implements OnInit {
 
-  rutas = ['/descripcion-parque', '/ubicacion-parque', '/piedras-parque', '/resenia-historica'];
+  rutas = ['/descripcion-parque', '/resenia-historica', '/ubicacion-parque', '/piedras-parque'];
   itemsInfo: ItemInfo[] = [];
 
   constructor(
