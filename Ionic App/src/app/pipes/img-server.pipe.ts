7f7dee69ab47e40c59d1bddb89c6b25446ba083e
Steven Cruz <blackjackers17@gmail.com>
2020-03-17 23:36:58 -0500

    modificando src de imagenes

--- Ionic App/src/app/pipes/img-server.pipe.ts
+++ Ionic App/src/app/pipes/img-server.pipe.ts
@@ -4,10 +4,11 @@ import { environment } from '../../environments/environment';
 @Pipe({
   name: 'imgServer'
 })
+
 export class ImgServerPipe implements PipeTransform {
 
-  transform(img: string, service: string): any {
-    const url = `${ environment.servicesAPI }/${ service }/?nombre=${img}`;
+  transform(img: string, tipo: string): any {
+    const url = `${ environment.servicesAPI }/images/getImage?tipo=${tipo}&nombre=${img}`;
     // console.log(url);
     return url;
   }
