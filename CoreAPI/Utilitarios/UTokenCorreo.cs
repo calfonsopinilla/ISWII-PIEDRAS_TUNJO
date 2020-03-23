1275d740e7199457ad07811629c34e6882543add
JhonattanPulido <jhony2010j222@gmail.com>
2020-03-23 12:49:09 -0500

    Modificacion de Agregar Usuario

--- CoreAPI/Utilitarios/UTokenCorreo.cs
+++ CoreAPI/Utilitarios/UTokenCorreo.cs
@@ -14,6 +14,13 @@ namespace Utilitarios {
         private DateTime fechaGeneracion;
         private DateTime fechaVencimiento;
         private int aplicacionId;
+        private string nombre;
+        private string apellido;
+        private string clave;
+        private string tipoDocumento;
+        private string numeroDocumento;
+        private string iconoUrl;
+        private int rolId;
 
         [Key]
         [Column("id")]
@@ -27,6 +34,20 @@ namespace Utilitarios {
         [Column("fecha_vencimiento")]
         public DateTime FechaVencimiento { get => fechaVencimiento; set => fechaVencimiento = value; }
         [Column("aplicacion_id")]
-        public int AplicacionId { get => aplicacionId; set => aplicacionId = value; }        
+        public int AplicacionId { get => aplicacionId; set => aplicacionId = value; }
+        [Column("nombre")]
+        public string Nombre { get => nombre; set => nombre = value; }
+        [Column("apellido")]
+        public string Apellido { get => apellido; set => apellido = value; }
+        [Column("clave")]
+        public string Clave { get => clave; set => clave = value; }
+        [Column("tipo_documento")]
+        public string TipoDocumento { get => tipoDocumento; set => tipoDocumento = value; }
+        [Column("numero_documento")]
+        public string NumeroDocumento { get => numeroDocumento; set => numeroDocumento = value; }
+        [Column("icono_url")]
+        public string IconoUrl { get => iconoUrl; set => iconoUrl = value; }
+        [Column("rol_id")]
+        public int RolId { get => rolId; set => rolId = value; }
     }
 }
