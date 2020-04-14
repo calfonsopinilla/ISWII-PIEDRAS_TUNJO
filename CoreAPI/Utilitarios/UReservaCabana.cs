81521e94e40d9c3be03b2cb836b364b2065471b7
Steven Cruz <blackjackers17@gmail.com>
2020-04-14 15:47:25 -0500

    imagenes cabana

--- CoreAPI/Utilitarios/UReservaCabana.cs
+++ CoreAPI/Utilitarios/UReservaCabana.cs
@@ -5,7 +5,7 @@ namespace Utilitarios {
 
     [Serializable] // Se declara que la clase U Usuario se puede expresar en formato JSON
     [Table("reserva_cabana", Schema = "parque")] // Se específica la tabla con la que se relaciona la clase U Usuario
-    class UReservaCabana : UReservaTicket {
+    class UReservaCabana {
 
     }
 }
