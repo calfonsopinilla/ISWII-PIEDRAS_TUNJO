5b7ad2ae14c88f2854584314c52b7f38bedd729a
JhonattanPulido <jhony2010j222@gmail.com>
2020-04-17 18:27:09 -0500

    Módulo de pago Stripe - Stripe Controller

---
+++ CoreAPI/Data/DAOStripe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilitarios;
+
+namespace Data {
+
+    /*
+        Author: Jhonattan Pulido
+        Creation Date: 17/04/2020
+        Description: Controller created for generate payments with Stripe
+    */
+    public class DAOStripe {
+
+        /*
+            Author: Jhonattan Pulido
+            Creation Date: 17/04/2020
+            Description: This method initialize the stripe config
+            Parameters: UStripe customerInfo: Object with the credential information declared
+            Returns: The var "customerInfo" with the secret API key & public API key declared
+        */
+        public UStripe InitConfig(UStripe customerInfo) {
+            
+            customerInfo.SecretApiKey = "sk_test_Kj64NJ6w0vZzdSYqj6ISaB5Z00KfR7Q05t";
+            customerInfo.PublicApiKey = "pk_test_9ghoZ0r96C1aUefVJ9iv1N9j00okeHjdPx";
+            return customerInfo;
+        }        
+    }
+}
---
+++ CoreAPI/Utilitarios/UStripe.cs
@@ -0,0 +1,31 @@
+﻿namespace Utilitarios {
+    /*
+        Author: Jhonattan Pulido
+        Creation Date: 17/04/2020
+        Description: Class created for declare the necesary attributes for generate payments with Stripe
+    */
+    public class UStripe {
+
+        // Vars
+        private string secretApiKey;
+        private string publicApiKey;
+        private string cardNumber;
+        private string monthExpiration;
+        private string yearExpiration;
+        private string cvc;
+        private string email;
+        private string token;
+        private long amount;
+
+        // Get & Set Methods
+        public string SecretApiKey { get => secretApiKey; set => secretApiKey = value; }
+        public string PublicApiKey { get => publicApiKey; set => publicApiKey = value; }
+        public string CardNumber { get => cardNumber; set => cardNumber = value; }
+        public string MonthExpiration { get => monthExpiration; set => monthExpiration = value; }
+        public string YearExpiration { get => yearExpiration; set => yearExpiration = value; }
+        public string Cvc { get => cvc; set => cvc = value; }
+        public string Email { get => email; set => email = value; }
+        public string Token { get => token; set => token = value; }
+        public long Amount { get => amount; set => amount = value; }
+    }
+}
