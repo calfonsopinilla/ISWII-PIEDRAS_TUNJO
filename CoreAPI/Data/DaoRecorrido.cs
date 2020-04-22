e9c2ea43e8870bf322926a13baf9501d30dca678
Steven Cruz <blackjackers17@gmail.com>
2020-04-22 00:48:36 -0500

    recorridos controller

--- CoreAPI/Data/DaoRecorrido.cs
+++ CoreAPI/Data/DaoRecorrido.cs
@@ -15,5 +15,37 @@ namespace Data
         {
             return db.Recorridos.ToList();
         }
+
+        public URecorrido Buscar(int id)
+        {
+            return db.Recorridos.Find(id);
+        }
+
+        public bool Agregar(URecorrido recorrido)
+        {
+            try
+            {
+                var created = db.Recorridos.Add(recorrido);
+                db.SaveChanges();
+                return created != null;
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Eliminar(int id)
+        {
+            try
+            {
+                var recorrido = db.Recorridos.Find(id);
+                db.Recorridos.Remove(recorrido);
+                db.SaveChanges();
+                return true;
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
--- CoreAPI/WebSite/PiedrasDelTunjo/PiedrasDelTunjo/Controllers/RecorridosController.cs
+++ CoreAPI/WebSite/PiedrasDelTunjo/PiedrasDelTunjo/Controllers/RecorridosController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using Utilitarios;
 
 namespace PiedrasDelTunjo.Controllers
 {
@@ -18,7 +19,34 @@ namespace PiedrasDelTunjo.Controllers
         public HttpResponseMessage ObtenerRecorridos()
         {
             var recorridos = new LRecorrido().ObtenerTodos();
-            return Request.CreateResponse(HttpStatusCode.OK, recorridos);
+            return Request.CreateResponse(HttpStatusCode.OK, new { ok = true, recorridos });
+        }
+
+        [HttpPost]
+        [Route("")]
+        public HttpResponseMessage Crear([FromBody] URecorrido recorrido)
+        {
+            if (recorrido == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { ok = false, message = "Bad request" });
+            }
+            recorrido.Token = Guid.NewGuid().ToString();
+            recorrido.LastModification = DateTime.Now;
+            bool created = new LRecorrido().Agregar(recorrido);
+            return Request.CreateResponse(HttpStatusCode.OK, new {ok  = true, message = "Created successfully!" });
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public HttpResponseMessage Eliminar([FromUri] int id)
+        {
+            var recorrido = new LRecorrido().Buscar(id);
+            if (recorrido == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { ok = false, message = "bad request" });
+            }
+            bool deleted = new LRecorrido().Eliminar(id);
+            return Request.CreateResponse(HttpStatusCode.OK, new { ok = deleted, message = "Deleted!" });
         }
     }
 }
