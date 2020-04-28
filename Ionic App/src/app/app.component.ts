7659f5ebc9553c3e5327454f2c44f63f7ec5b293
Jose Luis Soriano <joseluisoriano999@gmail.com>
2020-04-28 15:37:07 -0500

    Push

--- Ionic App/src/app/app.component.ts
+++ Ionic App/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Push, PushObject,PushOptions} from '@ionic-native/push/ngx';
+import {PushService} from  '../app/services/push.service';
+
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  
   constructor(
+    
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+    private statusBar: StatusBar,
+    private push : Push,
+    private pushService : PushService
+    ) {
     this.initializeApp();
   }
 
@@ -22,6 +28,57 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.pushSetup();
     });
   }
+
+  pushSetup(){
+
+    const options: PushOptions = {
+      android: {
+          senderID: '253556781690'
+      },
+      ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'true'
+      },
+      windows: {},
+      browser: {
+          pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+      }
+   };
+   
+   const pushObject: PushObject = this.push.init(options);
+   
+   pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+   
+
+
+
+   pushObject.on('registration').subscribe((registration: any ) =>  console.log('Device registered', registration));
+   
+   // insertar id del token 
+   pushObject.on('registration').subscribe((registration: any ) => this.insertarToken(registration.registrationId) );
+
+   pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+
+
+   
+
+  }
+
+
+  async insertarToken(token:string){
+    const push =  {
+        ObjetoPush : token,
+        Fecha : new Date(2000,1,1),
+        EstadoId : 1,
+        TokenId : token,
+    }
+    const respuest = this.pushService.agregarToken(push);
+    console.log(respuest);
+}
+
+
 }
---
+++ Ionic App/src/app/services/push.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { LoadingController, ToastController } from '@ionic/angular';
+
+const apiUrl = environment.servicesAPI;
+import { of, Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class PushService {
+  
+  constructor( private http: HttpClient,
+              private toastCtrl :ToastController
+    ) { }
+
+  agregarToken(push : any){
+    return this.http.post(`${ apiUrl }/push/crear`,push)
+                .pipe(catchError(err => {
+                    return of( err.error );
+                }))
+                .subscribe(res => {
+                    setTimeout(_ => {}, 2000);
+                    this.presentToast(res['message']);
+                },
+                (err) => {},
+            );
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+        message,
+        duration: 3000
+    });
+    toast.present();
+}
+}
